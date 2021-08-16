4d806112e3f48e9912e7afd166d9d904d87dbbc0
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-16 17:43:26 +0300

    feat: add search component styles

---
+++ src/components/Search/search.styles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  searchContainer: {
+    height: 40,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    width: '95%',
+    borderWidth: 0.3,
+  },
+  searchInput: {
+    height: 40,
+    width: '90%',
+    color: '#000',
+    paddingLeft: 15,
+  },
+  container: {
+    width: '10%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
